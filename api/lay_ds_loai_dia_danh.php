b76a205f178672824eed02f01389d8592a7a49a3
unknown <hoang.phan804@gmail.com>
2022-02-09 17:41:57 +0700

    add api

---
+++ api/lay_ds_loai_dia_danh.php
@@ -0,0 +1,13 @@
+<?php
+require('../db.php');
+$ds = $conn->prepare("SELECT * FROM loai_dia_danh ");
+$ds->execute();
+$ds->setFetchMode(PDO::FETCH_ASSOC);
+$result = $ds->fetchAll();
+$conn=null;
+$reTemp=$result["0"];
+$reTemp["id"]="0";
+$reTemp["ten"]="Tat ca";
+array_unshift($result,$reTemp);
+echo json_encode($result,JSON_UNESCAPED_UNICODE);
+?>
