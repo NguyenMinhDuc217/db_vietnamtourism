cc98019d994e5c5291f36a1955e1a2c7086dcefd
unknown <hoang.phan804@gmail.com>
2022-02-14 09:52:59 +0700

    api update

--- api/dang_ki.php
+++ api/dang_ki.php
@@ -8,15 +8,22 @@ $ktra = $conn->prepare("SELECT * FROM tai_khoan WHERE email='$username'");
 $ktra->execute();
 if ($ktra->rowCount() > 0) {
     $ktra->setFetchMode(PDO::FETCH_ASSOC);
-    $nhanVien = $ktra->fetchAll();    
+    $nhanVien = $ktra->fetchAll();
     echo json_encode($nhanVien);
     die();
 }
 
 $stmt = $conn->prepare("INSERT INTO tai_khoan(email, mat_khau,ten_nguoi_dung,loai_tai_khoan,trang_thai) VALUES('$username','$pass','$nick',2,1)");
 $stmt->execute();
-$nhanVien=$stmt->fetchAll();
+
+$ktra = $conn->prepare("SELECT * FROM tai_khoan WHERE email='$username'");
+$ktra->execute();
+$ktra->setFetchMode(PDO::FETCH_ASSOC);
+$nhanVien = $ktra->fetch();
+
+$id = $nhanVien["id"];
+$stmt = $conn->prepare("INSERT INTO trang_thai(tai_khoan_id, tt_email, tt_ten, tt_sdt) VALUES ($id,'1','1','2')");
+$stmt->execute();
+$result = $stmt->fetchAll();
 $conn = null;
-echo json_encode($nhanVien);
-
-
+echo json_encode($result);
--- api/lay_ds_dia_danh.php
+++ api/lay_ds_dia_danh.php
@@ -1,6 +1,7 @@
 <?php
 require('../db.php');
-$ds = $conn->prepare("SELECT * FROM dia_danh");
+//$ds = $conn->prepare("SELECT * FROM dia_danh");
+$ds = $conn->prepare("SELECT dia_danh.*, loai_dia_danh.ten as ten_loai_dia_danh FROM dia_danh,loai_dia_danh WHERE loai_dia_danh_id=loai_dia_danh.id ORDER BY sao_danh_gia DESC");
 $ds->execute();
 $ds->setFetchMode(PDO::FETCH_ASSOC);
 $result = $ds->fetchAll();
